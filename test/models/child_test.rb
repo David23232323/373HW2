4ffc30c8be3dd9728321b7c6daa5e4c3db56c810
David Cheung <davidcheung398@hotmail.com>
2021-02-04 12:44:02 -0500

    done with 4 and 5

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
+
 class ChildTest < ActiveSupport::TestCase
+
+  should have_many(:chores)
+  should have_many(:tasks).through(:chores)
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
   # TODO: Relationship macros
   
   # TODO: Validation macros
